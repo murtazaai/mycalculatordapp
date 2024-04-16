ff9396d76427743b70a49306e4da2e5208a0c447
Murtaza Ali Imtiaz <murtaza@kali.a>
2024-04-16 00:55:06 -0400

    Initiation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # A sample dapp.
+
+```
+anchor init mycalculatordapp
+cd mycalculatordapp
+npm install @project-serum/anchor @solana/web3.js
+anchor build
+```
--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -1,24 +1,28 @@
 import assert from 'assert';
 import anchor from '@project-serum/anchor';
-import SystemProgram from anchor.web3;
+import { SystemProgram } from anchor.web3;
 
 describe('mycalculatordapp', () => {
-  const provider = anchor.Provider.local();
+  const provider = anchor.AnchorProvider.local();
+
   anchor.setProvider(provider);
+  
   const calculator = anchor.web3.Keypair.generate();
+
   const program = anchor.workspace.MyCalculatorDapp();
 
   it('Creates a calculator', async() => {
     await program.rpc.create("My Calculator", {
       accounts: {
-        calculator: calculator.pubKey,
-        user: provider.wallet.pubKey,
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
         SystemProgram: SystemProgram.programId,
       },
       signers: [calculator]
     });
 
-    const account = await program.account.calculator.fetch(calculator.pubKey);
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    
     assert.ok(account.greeting === "My Calculator");
   });
 });
